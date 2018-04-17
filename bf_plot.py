c5f3907e40b1a2f81cbba50a270daf2cca90beed
jinyeom <jinyeom95@gmail.com>
2018-04-17 03:42:15 -0500

    added results

--- bf_plot.py
+++ bf_plot.py
@@ -5,15 +5,18 @@ from matplotlib import pyplot as plt
 
 def plot_nthreads_time(prog, filename, src, title, show=False, save=False):
     # plot the naive pthread implemenation of computation of pi with 1, 2, 4, 8 threads
-    print("Plotting runtimes of {} with 1, 2, 4, 8 threads...".format(prog))
+    print("Plotting speedups of {} with 1, 2, 4, 8 threads...".format(prog))
     speedups = []
 
+    print("Running {} on {} in serial...".format(prog, filename), flush=True, end="")
     result = subprocess.run([prog, "-f", filename, "-s", src], stdout=subprocess.PIPE).stdout.decode("utf-8")
+    print("\x1B[32mdone\x1B[0m")
     serial_runtime = float(re.search(r"elapsed process CPU time = (.*) nanoseconds", result).group(1))
-
-    for n in [1, 2, 4, 8]:
-        print("Running {} with {} thread(s)...".format(prog, n), flush=True, end="")
-        result = subprocess.run([prog, ], stdout=subprocess.PIPE).stdout.decode("utf-8")
+    
+    n_threads = [1, 2, 4, 8]
+    for n in n_threads:
+        print("Running {} on {} with {} thread(s)...".format(prog, filename, n), flush=True, end="")
+        result = subprocess.run([prog, "-f", filename, "-s", src, "-p", str(n)], stdout=subprocess.PIPE).stdout.decode("utf-8")
         print("\x1B[32mdone\x1B[0m")
         # first line of the result should have the runtime
         parallel_runtime = float(re.search(r"elapsed process CPU time = (.*) nanoseconds", result).group(1))
@@ -33,4 +36,7 @@ def plot_nthreads_time(prog, filename, src, title, show=False, save=False):
         print("\x1B[32mdone\x1B[0m")
         plt.close()
 
-plot_nthreads_time("./bellman-ford", "/tmp/jinyeom/rmat15.dimacs", , save=True)
+plot_nthreads_time("./bellman-ford", "/tmp/jinyeom/rmat15.dimacs", "1", "rmat15", save=True)
+plot_nthreads_time("./bellman-ford", "/tmp/jinyeom/rmat22.dimacs", "1", "rmat22", save=True)
+plot_nthreads_time("./bellman-ford", "/tmp/jinyeom/roadNY.dimacs", "140961", "roadNY", save=True)
+plot_nthreads_time("./bellman-ford", "/tmp/jinyeom/roadFLA.dimacs", "316607", "roadFLA", save=True)
